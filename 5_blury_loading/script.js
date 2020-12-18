3f53cf8a0e7c297eafbb54fffe1c673deaa499d1
bronwyncarr <bronwynacarr@gmail.com>
2020-12-18 21:45:09 +1100

    formatted

--- 5_blury_loading/script.js
+++ 5_blury_loading/script.js
@@ -1,22 +1,21 @@
 (function () {
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
-const heading = document.querySelector('.heading')
-let load = 0
-let int = setInterval(blurring, 30)
+  const loadText = document.querySelector(".loading-text");
+  const bg = document.querySelector(".bg");
+  const heading = document.querySelector(".heading");
+  let load = 0;
+  let int = setInterval(blurring, 30);
 
+  function blurring() {
+    load++;
+    if (load > 99) {
+      clearInterval(int);
+    }
 
-function blurring() {
-  load++
-  if (load > 99) {
-    clearInterval(int)
+    loadText.innerText = `${load}%`;
+    loadText.style.opacity = (100 - load) / 100;
+    bg.style.filter = `blur(${(100 - load) / 3.3}px)`;
   }
-
-  loadText.innerText = `${load}%`
-  loadText.style.opacity = (100-load) /100;
-  bg.style.filter = `blur(${(100-load)/3.3}px)`
-}
-})()
+})();
 
 // mapping one number to another number function
 // const scale = (num, in_min, in_max, out_min, out_max) => {
