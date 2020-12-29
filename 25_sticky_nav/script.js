de873a9a25bd82f1571847ae9047f24dc35b7902
bronwyncarr <bronwynacarr@gmail.com>
2020-12-29 16:59:31 +1100

    refactor function variables

--- 25_sticky_nav/script.js
+++ 25_sticky_nav/script.js
@@ -1,11 +1,10 @@
-const nav = document.querySelector('.nav')
-
-window.addEventListener('scroll', fixNav)
-
 function fixNav() {
+  const nav = document.querySelector('.nav')
   if (window.scrollY > nav.offsetHeight + 150) {
     nav.classList.add('active')
   } else {
     nav.classList.remove('active')
   }
 }
+
+window.addEventListener('scroll', fixNav)
